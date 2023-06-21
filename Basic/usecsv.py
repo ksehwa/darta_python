6d45c05ebd6f08dd3e53220f4c161b876876405d
ksehwa <kke86624@gmail.com>
2023-06-21 11:33:27 +0900

    230620

--- Basic/usecsv.py
+++ Basic/usecsv.py
@@ -1,7 +1,7 @@
 import csv, re
 
 def opencsv(filename):
-    f= open(filename, 'r')
+    f= open(filename, 'r', encoding = 'cp949')
     reader = csv.reader(f)
     output = []
     for i in reader:
@@ -10,7 +10,7 @@ def opencsv(filename):
 
 
 def writecsv(filename, the_list):                  # 첫 번째 변수는 파일이름, 두번째 변수는 CSV형 리스트를 저장한 객체 
-    with open(filename, 'w', newline='') as f:     # 파일을 닫기 위해 whitt문 사용
+    with open(filename, 'w', newline='', encoding = 'cp949') as f:     # 파일을 닫기 위해 whitt문 사용
         a = csv.writer(f, delimiter=',')
         a.writerows(the_list)
 
